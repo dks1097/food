bac7ac8ab747659ab4fec8ca75507f5d273789ad
dks1097 <carlos.d.t.m19@gmail.com>
2022-05-08 20:42:21 +0100

    setting up the food App

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View,Text, StyleSheet} from "react-native";
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text style={styles.text}>SearchScreen</Text>
+        </View>
+    );
+};
+const styles = StyleSheet.create({});
+export default SearchScreen;
